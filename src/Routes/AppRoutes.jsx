47c6f1a09e8ebcc294db431f1fc91d8fb055c50a
Jorge Segovia <jorge.segovia@epn.edu.ec>
2024-11-25 18:23:42 -0500

    chore: delete trash files

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,4 +1,3 @@
-// src/routes/AppRoutes.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CartProvider } from "../components/CartContext";
