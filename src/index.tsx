0e428e267361e302c69913f246ef3615804e4ff9
Jorge Segovia <jorge.segovia@epn.edu.ec>
2025-01-13 10:45:38 -0500

    Fix: index.tsx routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
-import App from 'components/App'
+import App from './components/App'
+import React from 'react'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
